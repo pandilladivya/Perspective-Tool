2ad7df89b5dae40df38d7effde2e6221fb7f6bd0
Divya Pandilla <divya.pandilla8@gmail.com>
2019-07-25 20:23:24 +0530

    Sample Question

---
+++ frontend/src/pages/HomePage/Question/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Radio, Card, List } from 'antd' 
+
+class Question extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      question: this.props.question
+    } 
+  }
+  onChange = e => { 
+    this.setState({
+      value: e.target.value,
+    });
+  };
+  render () {
+    return (
+      <Card> 
+        {this.state.question}
+        <Radio.Group onChange={this.onChange} value={this.state.value}>
+        <Radio value={1}></Radio>
+        <Radio value={2}></Radio>
+        <Radio value={3}></Radio>
+        <Radio value={4}></Radio>
+        <Radio value={5}></Radio>
+        <Radio value={6}></Radio>
+        <Radio value={7}></Radio>
+      </Radio.Group>
+      </Card>
+    )
+  }
+}
+
+export default Question
