0b4fa9b17e619311080ab6c43f3febbd6c7406ef
Divya Pandilla <divya.pandilla8@gmail.com>
2019-07-25 19:38:34 +0530

    Selectors

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -4,14 +4,19 @@ import { createStructuredSelector } from 'reselect'
 
 import HomeComponent from './HomeComponent'
   
+import { listQuestions } from '../../modules/questions'
+
+import {makeSelectQuestions} from '../../selectors/questions'
 
 const mapStateToProps = createStructuredSelector({ 
+  questions: makeSelectQuestions()
 })
 
 function mapDispatchToProps (dispatch) {
   return {
     actions: bindActionCreators(
       { 
+        listQuestions
       },
       dispatch
     )
