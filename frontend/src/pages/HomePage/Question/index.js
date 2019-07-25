b47115fb9b6a62ed991ad219eb8452eb5b8876cc
Divya Pandilla <divya.pandilla8@gmail.com>
2019-07-26 00:05:03 +0530

    UI for questions

--- frontend/src/pages/HomePage/Question/index.js
+++ frontend/src/pages/HomePage/Question/index.js
@@ -1,34 +1,65 @@
 import React from 'react'
-import { Radio, Card, List } from 'antd' 
+import { Radio, Card, List } from 'antd'
+import styled from 'styled-components'
 
+const RadioComponent = styled(Radio)`
+  margin: 10px;
+`
+const DisagreeText = styled.span`
+    color: red;
+    font-weight: 500;
+    font-size: 14px;
+    `
+
+const AgreeText = styled.span`
+    color: green;
+    font-weight: 500;
+    font-size: 14px;
+    `
+const InnerLayout = styled.div`
+margin: 40px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
 class Question extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       question: this.props.question
-    } 
+    }
   }
-  onChange = e => { 
+  onChange = e => {
     this.setState({
       value: e.target.value,
     });
   };
-  render () {
+  render() {
     return (
       <Card style={{
-        margin: 40,
+        marginLeft: 40,
+        marginRight: 40,
+        width: 600,
         padding: 40,
-        boxShadow: '0 0 0 1px #e5e5e5'}}> 
+        boxShadow: '0 0 0 1px #e5e5e5',
+        textAlign: 'center',
+        fontWeight: 600
+      }}>
         {this.state.question}
-        <Radio.Group onChange={this.onChange} value={this.state.value}>
-        <Radio value={1}></Radio>
-        <Radio value={2}></Radio>
-        <Radio value={3}></Radio>
-        <Radio value={4}></Radio>
-        <Radio value={5}></Radio>
-        <Radio value={6}></Radio>
-        <Radio value={7}></Radio>
-      </Radio.Group>
+        <InnerLayout>
+          <DisagreeText>Disagree</DisagreeText>
+          <Radio.Group size="large" onChange={this.onChange} value={this.state.value}>
+            <RadioComponent value={1}></RadioComponent>
+            <RadioComponent value={2}></RadioComponent>
+            <RadioComponent value={3}></RadioComponent>
+            <RadioComponent value={4}></RadioComponent>
+            <RadioComponent value={5}></RadioComponent>
+            <RadioComponent value={6}></RadioComponent>
+            <RadioComponent value={7}></RadioComponent>
+          </Radio.Group>
+          <AgreeText>Agree</AgreeText>
+        </InnerLayout>
+
       </Card>
     )
   }
